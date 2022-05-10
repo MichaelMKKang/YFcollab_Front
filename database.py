ac570387f8f6ed98ee8591f4a5cda0ded12bbe0e
Kang, Michael (New York - US) <Michael.Kang@GLENCORE-US.COM>
2022-05-10 14:46:00 -0400

    adding sample postgres db

---
+++ database.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASE_URL = \
+        'postgresql://{username}:{password}@{host}:{port}/{database}'.format(
+            username=os.environ['RDS_USERNAME'],
+            password=os.environ['RDS_PASSWORD'],
+            host=os.environ['RDS_HOSTNAME'],
+            port=os.environ['RDS_PORT'],
+            database=os.environ['RDS_DB_NAME'],
+        )
+else:
+    DATABASE_URL = \
+        'postgresql://{username}:{password}@{host}:{port}/{database}'.format(
+            username='mkang',
+            password='Polarbear5684',
+            host='localhost',
+            port='5432',
+            database='fastapi-product',
+        )
+
+engine = create_engine(
+    DATABASE_URL,
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+import models
+from database import engine, SessionLocal
+from models import Song
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/')
+def read_root():
+    return {'name': 'fastapi-songs'}
+
+
+@app.get('/songs/')
+def read_songs(db: Session = Depends(get_db)):
+    return db.query(Song).all()
